aea15613ba5e7c29849d69772542d7883622b265
Tjalle Borgers <tjalle@borgersonline.nl>
2024-02-27 10:09:16 +0100

    turn system implemented, highly jank, will need to refactor when alex is done with the events

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -1,10 +1,31 @@
 using Mirror;
+using UnityEngine;
 
 public class TurnManager : NetworkBehaviour
 {
+    [SerializeField, SyncVar] private Player activePlayer;
+    [SerializeField, SyncVar] private int activePlayerIndex;
+    [SerializeField, SyncVar] private SyncList<Player> players;
+
+    public Player ActivePlayer => activePlayer;
+    
     [Command(requiresAuthority = false)]
-    public void NextTurn()
+    public void NextTurn(Player player)
     {
+        if(player != activePlayer)
+            return;
+
+        activePlayerIndex++;
+        if (activePlayerIndex >= players.Count)
+            activePlayerIndex = 0;
+
+        activePlayer = players[activePlayerIndex];
+        
         EventBus<OnStartTurn>.Publish(new OnStartTurn());
+    }
+
+    public void RegisterNewPlayer(Player player)
+    {
+        players.Add(player);
     }
 }
